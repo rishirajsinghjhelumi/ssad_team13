ae4c38b2b007fc610423b1b7b5e51fb4a0b1b984
Anhad Jai Singh <anhadjai.singh@Black.Knight>
2012-10-17 00:30:30 +0530

    Integrated Logging, Accelration, Calibration and Network Checks

---
+++ Integrated_App/src/com/example/testapp/Gps.java
@@ -0,0 +1,245 @@
+package com.example.testapp;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class Gps extends Activity {
+	
+	private LocationManager locman;
+	private boolean isGPSEnabled=false;
+	private NetworkInfo networkInfo;
+	private ConnectivityManager conman;
+	private boolean isNetworkEnabled=false;
+	
+	/*
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		check
+		setContentView(R.layout.activity_gps);
+	}
+	
+	public void redirectGps(View view)
+	{
+		Intent intent = new Intent(Intent.ACTION_MAIN);
+		intent.setClassName("com.android.settings","com.android.settings.SecuritySettings");
+		startActivity(intent);
+	}
+	
+	public void redirectNetwork(View view)
+	{
+		Intent intent2 = new Intent(Intent.ACTION_MAIN);
+		intent2.setClassName("com.android.phone", "com.android.phone.Settings");
+		startActivity(intent2);
+	}
+	
+	public void check()
+	{
+		locman=(LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        isGPSEnabled = locman.isProviderEnabled(LocationManager.GPS_PROVIDER);
+        
+        conman = (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
+        networkInfo = conman.getActiveNetworkInfo();
+        
+        if(networkInfo!=null)
+        {
+        	isNetworkEnabled = networkInfo.isConnected();
+        }
+        else
+        {
+        	isNetworkEnabled = false;
+        }
+        }
+	}
+*/
+	final Context context = this;
+	@Override
+	public void onResume()
+    { 
+		super.onResume();
+		final TextView tv3 = (TextView) findViewById(R.id.textView3);
+		locman=(LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+		isGPSEnabled = locman.isProviderEnabled(LocationManager.GPS_PROVIDER);
+		if(!isGPSEnabled)
+		{	
+			tv3.setText("GPS: ");
+			Button b = (Button) findViewById(R.id.button1);
+			b.setOnClickListener(new OnClickListener() {
+				public void onClick(View v) {
+					Intent intent = new Intent(Intent.ACTION_MAIN);
+					intent.setClassName("com.android.settings","com.android.settings.SecuritySettings");
+					startActivity(intent);
+				}
+			});
+		}
+		else
+		{
+			final Button b = (Button) findViewById(R.id.button1);
+			tv3.setText("GPS: ");
+			b.setText("Enabled");
+			b.setOnClickListener(new OnClickListener() {
+				public void onClick(View v) {
+					AlertDialog.Builder alertdialog = new AlertDialog.Builder(context);
+					alertdialog.setTitle("GPS..");
+					alertdialog.setMessage("GPS Already Enabled!!!")
+								.setCancelable(false)
+								.setNegativeButton("OK", new DialogInterface.OnClickListener() 
+								{
+									public void onClick(DialogInterface dialog, int which) 
+									{
+										dialog.cancel();
+									}
+								}
+						);
+					AlertDialog alertDialog2 = alertdialog.create();
+					alertDialog2.show();
+				}
+			});
+		}
+		TextView tv4 = (TextView) findViewById(R.id.textView4);
+		ConnectivityManager conman = (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
+		networkInfo = conman.getActiveNetworkInfo();
+		if(networkInfo != null && networkInfo.isConnected())
+		{
+			final Button b2 = (Button) findViewById(R.id.button2);
+			tv4.setText("Network: ");
+			b2.setText("Enabled");
+			b2.setOnClickListener(new OnClickListener() {
+				public void onClick(View v) {
+					AlertDialog.Builder alertdialog = new AlertDialog.Builder(context);
+					alertdialog.setTitle("Network..");
+					alertdialog.setMessage("Network Already Enabled!!!")
+								.setCancelable(false)
+								.setNegativeButton("OK", new DialogInterface.OnClickListener() 
+								{
+									public void onClick(DialogInterface dialog, int which) 
+									{
+										dialog.cancel();
+									}
+								}
+						);
+					AlertDialog alertDialog2 = alertdialog.create();
+					alertDialog2.show();
+				}
+			});
+		}
+		else
+		{
+			tv4.setText("Network: ");
+			Button b2 = (Button) findViewById(R.id.button2);
+			b2.setOnClickListener(new OnClickListener() {
+				public void onClick(View v) {
+					Intent intent2 = new Intent(Intent.ACTION_MAIN);
+					intent2.setClassName("com.android.phone", "com.android.phone.Settings");
+					startActivity(intent2);
+				}
+			});
+		}
+		if(isGPSEnabled && networkInfo!=null && networkInfo.isConnected() )
+		{
+			Log.i("sensorIFCHECK", "GOING inside IF");
+			Intent intentnext = new Intent(Gps.this,AskScreen.class);
+			startActivity(intentnext);
+		}
+     }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_gps);
+		final TextView tv3 = (TextView) findViewById(R.id.textView3);	
+		locman=(LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+		isGPSEnabled = locman.isProviderEnabled(LocationManager.GPS_PROVIDER);
+		if(!isGPSEnabled)
+		{
+			tv3.setText("GPS: ");
+			Button b = (Button) findViewById(R.id.button1);
+			b.setOnClickListener(new OnClickListener() {
+				public void onClick(View v) {
+					Intent intent = new Intent(Intent.ACTION_MAIN);
+					intent.setClassName("com.android.settings","com.android.settings.SecuritySettings");
+					startActivity(intent);
+				}
+			});
+		}
+		else
+		{
+			final Button b = (Button) findViewById(R.id.button1);
+			tv3.setText("GPS: ");
+			b.setText("Enabled");
+			b.setOnClickListener(new OnClickListener() {
+				public void onClick(View v) {
+					AlertDialog.Builder alertdialog = new AlertDialog.Builder(context);
+					alertdialog.setTitle("GPS..");
+					alertdialog.setMessage("GPS Already Enabled!!!")
+								.setCancelable(false)
+								.setNegativeButton("OK", new DialogInterface.OnClickListener() 
+								{
+									public void onClick(DialogInterface dialog, int which) 
+									{
+										dialog.cancel();
+									}
+								}
+						);
+					AlertDialog alertDialog2 = alertdialog.create();
+					alertDialog2.show();
+				}
+			});
+		}
+		TextView tv4 = (TextView) findViewById(R.id.textView4);
+		ConnectivityManager conman = (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
+		networkInfo = conman.getActiveNetworkInfo();
+		if(networkInfo != null && networkInfo.isConnected())
+		{
+			final Button b2 = (Button) findViewById(R.id.button2);
+			tv4.setText("Network: ");
+			b2.setText("Enabled");
+			b2.setOnClickListener(new OnClickListener() {
+				public void onClick(View v) {
+					AlertDialog.Builder alertdialog = new AlertDialog.Builder(context);
+					alertdialog.setTitle("Network..");
+					alertdialog.setMessage("Network Already Enabled!!!")
+								.setCancelable(false)
+								.setNegativeButton("OK", new DialogInterface.OnClickListener() 
+								{
+									public void onClick(DialogInterface dialog, int which) 
+									{
+										dialog.cancel();
+									}
+								}
+						);
+					AlertDialog alertDialog2 = alertdialog.create();
+					alertDialog2.show();
+				}
+			});
+		}
+		else
+		{
+			tv4.setText("Network: ");
+			Button b2 = (Button) findViewById(R.id.button2);
+			b2.setOnClickListener(new OnClickListener() {
+				public void onClick(View v) {
+					Intent intent2 = new Intent(Intent.ACTION_MAIN);
+					intent2.setClassName("com.android.phone", "com.android.phone.Settings");
+					startActivity(intent2);
+				}
+			});
+		}
+		if(isGPSEnabled && networkInfo!=null && networkInfo.isConnected())
+		{
+			Intent intentnext = new Intent(Gps.this,AskScreen.class);
+			startActivity(intentnext);
+		}
+	}
+}
---
+++ Integrated_App/src/com/example/testapp/MainActivity.java
@@ -0,0 +1,65 @@
+package com.example.testapp;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
+import android.location.LocationManager;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+
+
+public class MainActivity extends Activity {
+	//public static String EX = "ThisIsKey";
+	private boolean isGPSEnabled;
+	private NetworkInfo networkInfo;
+	
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        LocationManager locman=(LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        isGPSEnabled = locman.isProviderEnabled(LocationManager.GPS_PROVIDER);
+        
+        ConnectivityManager conman = (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
+        networkInfo = conman.getActiveNetworkInfo();
+        
+        final SQLiteDatabase database = this.openOrCreateDatabase("Logs", MODE_PRIVATE, null);
+        database.execSQL("CREATE TABLE IF NOT EXISTS " +
+                " SPEED_VIOLATIONS " +
+                " (DATE VARCHAR, TIME VARCHAR," +
+                " VIOLATION_TYPE VARCHAR );");
+              
+    }
+
+    public void sendMessage(View view)
+    {
+    	       
+        if(isGPSEnabled && networkInfo.isConnected() && networkInfo!=null)
+        {
+        	Intent intent = new Intent (MainActivity.this, AskScreen.class);
+        	startActivity(intent);
+        }
+        else
+        {
+        	Intent altintent = new Intent(MainActivity.this,Gps.class);
+        	//Intent altintent = new Intent (MainActivity.this, AskScreen.class);
+        	startActivity(altintent);
+        }
+        
+    	
+    }
+    
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
+    }
+
+}
+
+/**/
